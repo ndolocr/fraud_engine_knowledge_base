39b7405b6a57b20a24a2342ccee05943c39069ab
ndolocr <crndolo@gmail.com>
2024-11-12 12:09:05 +0300

    Creating Rules Model

--- KnowledgeBase/models.py
+++ KnowledgeBase/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Rule(models.Model):
+    class Meta:
+        db_table = "rules"
+
+    action = models.TextField()
+    conditions = models.TextField()
+    priority = models.IntegerField()
+    description = models.TextField()
+    rule_id = models.CharField(max_length = 512)
+    ruleNamespace = models.CharField(max_length = 256)
+
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
