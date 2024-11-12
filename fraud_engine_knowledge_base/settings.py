851093261e97acce23ace57fc14e47693ee065ee
ndolocr <crndolo@gmail.com>
2024-11-12 12:03:18 +0300

    Update Installed APPs.

--- fraud_engine_knowledge_base/settings.py
+++ fraud_engine_knowledge_base/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps
+
+    # Third Party Apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
